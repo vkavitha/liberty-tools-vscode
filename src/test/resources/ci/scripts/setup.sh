6ad97499de1f177f84c133635628ddb19c89aca9
KAVITHA VARADARAJAN <10922675+vkavitha@users.noreply.github.com>
2023-03-14 09:41:10 +0530

    run docker on mac

--- src/test/resources/ci/scripts/setup.sh
+++ src/test/resources/ci/scripts/setup.sh
@@ -2,13 +2,13 @@
 
 ############################################################################
 # Copyright (c) 2022 IBM Corporation and others.
-# 
+#
 # This program and the accompanying materials are made available under the
 # terms of the Eclipse Public License v. 2.0 which is available at
 # http://www.eclipse.org/legal/epl-2.0.
-# 
+#
 # SPDX-License-Identifier: EPL-2.0
-# 
+#
 # Contributors:
 #     IBM Corporation - initial implementation
 ############################################################################
@@ -20,7 +20,7 @@ OS=$(uname -s)
 
 # Semeru JDK version control constants.
 # Example:
-# Version (SEMERU_OPEN_JDK_BUILD): 11.0.14.1 
+# Version (SEMERU_OPEN_JDK_BUILD): 11.0.14.1
 # OpenJDK (SEMERU_OPEN_JDK_BUILD + SEMERU_OPEN_JDK_BUILD): 11.0.14.1+1
 # OpenJ9  (SEMERU_OPENJ9_VERSION): 0.30.1
 SEMERU_OPEN_JDK_MAJOR=17
@@ -51,10 +51,10 @@ main() {
         echo "ERROR: OS $OS is not supported."
         exit -1
     fi
-    
+
     # Create install directory.
     mkdir -p "$SOFTWARE_INSTALL_DIR"
-    
+
     # Install all required software.
     installBaseSoftware
     installCustomSoftware
@@ -67,15 +67,16 @@ main() {
 installBaseSoftware() {
     if [[ $OS == "Linux" ]]; then
         sudo apt-get update
-        sudo apt-get install curl unzip 
+        sudo apt-get install curl unzip
         installXDisplaySoftwareOnLinux
         installDockerOnLinux
     elif [[ $OS == "Darwin" ]]; then
         brew update
         brew install curl unzip || true
         brew install docker
+        open -a docker
     else
-        # Note: Docker is already installed on the windows VMs provisioned by GHA. 
+        # Note: Docker is already installed on the windows VMs provisioned by GHA.
         # Location: C:\Program Files\Docker\dockerd.exe
         choco install curl
         choco install unzip
@@ -83,11 +84,11 @@ installBaseSoftware() {
 }
 
 # installSoftware installs customizable software.
-installCustomSoftware() {	
+installCustomSoftware() {
 	installJDK
 	installMaven
 	installGradle
-}  
+}
 
 # installJDK installs the set version of the Semeru JDK.
 installJDK() {
@@ -97,13 +98,13 @@ installJDK() {
 	if [[ $OS == "Linux" ]]; then
         local url="https://github.com/ibmruntimes/semeru${SEMERU_OPEN_JDK_MAJOR}-binaries/releases/download/jdk-${SEMERU_OPEN_JDK_VERSION}%2B${SEMERU_OPEN_JDK_BUILD}_openj9-${SEMERU_OPENJ9_VERSION}/ibm-semeru-open-jdk_x64_linux_${SEMERU_OPEN_JDK_VERSION}_${SEMERU_OPEN_JDK_BUILD}_openj9-${SEMERU_OPENJ9_VERSION}.tar.gz"
         curl -fsSL -o /tmp/liberty-dev-tool-semeru-jdk.tar.gz "$url"
-        echo "${SEMERU_ARCHIVE_LINUX_SHA256}  /tmp/liberty-dev-tool-semeru-jdk.tar.gz" | sha256sum -c - 
+        echo "${SEMERU_ARCHIVE_LINUX_SHA256}  /tmp/liberty-dev-tool-semeru-jdk.tar.gz" | sha256sum -c -
         tar -xzf /tmp/liberty-dev-tool-semeru-jdk.tar.gz -C "$SOFTWARE_INSTALL_DIR"
     elif [[ $OS == "Darwin" ]]; then
        javaHome="$javaHome"/Contents/Home
        local url="https://github.com/ibmruntimes/semeru${SEMERU_OPEN_JDK_MAJOR}-binaries/releases/download/jdk-${SEMERU_OPEN_JDK_VERSION}%2B${SEMERU_OPEN_JDK_BUILD}_openj9-${SEMERU_OPENJ9_VERSION}/ibm-semeru-open-jdk_x64_mac_${SEMERU_OPEN_JDK_VERSION}_${SEMERU_OPEN_JDK_BUILD}_openj9-${SEMERU_OPENJ9_VERSION}.tar.gz"
        curl -fsSL -o /tmp/liberty-dev-tool-semeru-jdk.tar.gz "$url"
-       echo "${SEMERU_ARCHIVE_MAC_SHA256}  /tmp/liberty-dev-tool-semeru-jdk.tar.gz" | shasum -a 256 -c - 
+       echo "${SEMERU_ARCHIVE_MAC_SHA256}  /tmp/liberty-dev-tool-semeru-jdk.tar.gz" | shasum -a 256 -c -
        tar -xzf /tmp/liberty-dev-tool-semeru-jdk.tar.gz -C "$SOFTWARE_INSTALL_DIR"
     else
         local url="https://github.com/ibmruntimes/semeru${SEMERU_OPEN_JDK_MAJOR}-binaries/releases/download/jdk-${SEMERU_OPEN_JDK_VERSION}%2B${SEMERU_OPEN_JDK_BUILD}_openj9-${SEMERU_OPENJ9_VERSION}/ibm-semeru-open-jdk_x64_windows_${SEMERU_OPEN_JDK_VERSION}_${SEMERU_OPEN_JDK_BUILD}_openj9-${SEMERU_OPENJ9_VERSION}.zip"
@@ -134,9 +135,9 @@ installMaven() {
 
 	# Check the downloaded archive's SHA against the expected value.
 	if [[ $OS == "Linux" ]]; then
-        echo "${MAVEN_ARCHIVE_SHA512}  /tmp/liberty-dev-tool-apache-maven.zip" | sha512sum -c - 	
+        echo "${MAVEN_ARCHIVE_SHA512}  /tmp/liberty-dev-tool-apache-maven.zip" | sha512sum -c -
     elif [[ $OS == "Darwin" ]]; then
-        echo "${MAVEN_ARCHIVE_SHA512}  /tmp/liberty-dev-tool-apache-maven.zip" | shasum -a 512 -c - 
+        echo "${MAVEN_ARCHIVE_SHA512}  /tmp/liberty-dev-tool-apache-maven.zip" | shasum -a 512 -c -
     else
         local shaAll=$(certutil -hashfile /tmp/liberty-dev-tool-apache-maven.zip SHA512)
         local downloadedZipSha=$(echo $(echo $shaAll | tr '\r' ' ') | cut -d " " -f 5)
@@ -160,7 +161,7 @@ installGradle() {
 
     # Download the Gradle archive.
     curl -fsSL -o /tmp/liberty-dev-tool-gradle.zip "$url"
-      
+
     # Check the downloaded archive's SHA against the expected value.
 	if [[ $OS == "Linux" ]]; then
         echo "${GRADLE_ARCHIVE_SHA256}  /tmp/liberty-dev-tool-gradle.zip" | sha256sum -c -
@@ -184,8 +185,8 @@ installGradle() {
 
 # installXDisplaySoftwareOnLinux Installs a X display, a windows manager, and other pre-req software.
 installXDisplaySoftwareOnLinux() {
-    sudo apt-get install dbus-x11 xvfb metacity at-spi2-core 
-} 
+    sudo apt-get install dbus-x11 xvfb metacity at-spi2-core
+}
 
 # installDocker installs docker.
 installDockerOnLinux() {
